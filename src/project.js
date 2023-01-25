ddf8c06e09a8c997e0049ceb4cef7abeae1409e2
Samuel Evans <117552758+JSamuel38@users.noreply.github.com>
2023-01-25 21:19:23 +0000

    Add remove button styling

--- src/project.js
+++ src/project.js
@@ -17,19 +17,24 @@ function createProjectCard(project) {
   const title = document.createElement('h1');
   const dueDate = document.createElement('p');
   const priority = document.createElement('p');
+  const removeBtn = document.createElement('button');
 
   card.classList.add('bg-blue-600', 'rounded-lg', 'p-2', 'text-white', 'text-center', 'break-words', 'flex', 'justify-center', 'flex-col', 'items-center');
   title.classList.add('font-bold', 'text-2xl');
   dueDate.classList.add('text-xl');
   priority.classList.add('text-xl');
+  removeBtn.classList.add('bg-red-600', 'text-white', 'rounded', 'px-4', 'py-2', 'm-4', 'hover:bg-red-700', 'active:bg-red-800');
+
 
   title.innerText = project.title;
   dueDate.innerText = `Due: ${project.dueDate}`;
   priority.innerText = `Priority: ${project.priority}`;
+  removeBtn.innerText = 'Remove';
 
   card.appendChild(title);
   card.appendChild(dueDate);
   card.appendChild(priority);
+  card.appendChild(removeBtn);
 
   return card;
 }
